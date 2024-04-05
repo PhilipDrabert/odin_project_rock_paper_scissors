12b2a6ff7cacff19b7ad266c53d5b06bcf82e6d9
Philip Drabert <philipdrabert@Philips-MacBook-Pro.local>
2024-04-05 17:26:39 +0200

    The JavaScript game logic is completed and working.

---
+++ script.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const options = ["Rock", "Paper", "Scissors"];
+    let playerSelection = options[0];
+    const getComputerChoice = function() {
+        let randomNumber = Math.floor(Math.random() * 3);
+        return options[randomNumber];
+    }
+    let computerSelection = getComputerChoice();
+    const playRound = function(playerSelection, computerSelection) {
+        if (playerSelection === computerSelection) {
+            return "It's a tie!"
+        }
+        else {
+            switch (playerSelection) {
+                case options[0]:
+                    if (computerSelection === options[1]) {
+                        return "You lose!";
+                    } else {
+                        return "You win!";
+                    }
+                case options[1]:
+                    if (computerSelection === options[2]) {
+                        return "You lose!";
+                    } else {
+                        return "You win!";
+                    }
+                case  options[2]:
+                    if (computerSelection === options[0]) {
+                        return "You lose!";
+                    } else {
+                        return "You win!";
+                    }
+            }
+        }
+    } 
+   console.log(computerSelection)
+   console.log(playRound(playerSelection, computerSelection))
+});
